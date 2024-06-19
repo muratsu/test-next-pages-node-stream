943fea7d82ddb796150d689764bdbf1dd09fe500
Murat Sutunc <sutunc@gmail.com>
2024-06-19 12:02:32 -0700

    headers

--- app/api/app-route/route.ts
+++ app/api/app-route/route.ts
@@ -11,7 +11,7 @@ export const dynamic = "force-dynamic";
 
 const messages =
   `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet`.split(
-    ".",
+    "."
   );
 
 export async function GET() {
@@ -33,6 +33,9 @@ export async function GET() {
   });
 
   return new Response(customReadable, {
-    headers: { "Content-Type": "text/plain; charset=utf-8" },
+    headers: {
+      "Content-Type": "text/event-stream",
+      "X-Content-Type-Options": "nosniff",
+    },
   });
 }
